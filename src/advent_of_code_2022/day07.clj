14a4db28102dc3047d4ed388bab33873b6bd0cae
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2022-12-07 09:19:41 +0100

    feat: Add solution for day 07

---
+++ src/advent_of_code_2022/day07.clj
@@ -0,0 +1,76 @@
+(ns advent-of-code-2022.day07
+  (:require [advent-of-code-2022.utils :refer [read-input-as-string-vector]]
+            [clojure.string :as str]))
+
+(defn- get-top-level-dir
+  [current-dir]
+  (let [tmp-dir (->> (str/split current-dir #"/")
+                     (drop-last)
+                     (str/join "/"))]
+    (if (str/blank? tmp-dir) "/" tmp-dir)))
+
+(defn- process-command-log
+  [data]
+  (loop [dirs (sorted-map)
+         log data
+         current-dir "/"]
+    (if (empty? log)
+          dirs
+          (let [line (first log)
+                cd (second (re-matches #"\$ cd ([/.a-z]+)" line))
+                file (second (re-matches #"(\d+) .+" line))]
+            (cond
+              (some? file) (let [size (Integer/parseInt file)]
+                             (recur (assoc dirs current-dir (+ (get dirs current-dir 0) size)) (rest log) current-dir))
+              (some? cd) (case cd
+                           "/" (recur dirs (rest log) "/")
+                           ".." (recur dirs (rest log) (get-top-level-dir current-dir))
+                           (recur dirs (rest log) (if (= current-dir "/") (str "/" cd) (str current-dir "/" cd))))
+              :else (recur dirs (rest log) current-dir))))))
+
+(defn- update-directory-sizes
+  [dirs current-dir]
+  (let [size (get dirs current-dir 0)]
+    (if (or (= size 0) (= current-dir "/"))
+      dirs
+      (loop [dirs dirs
+             current-dir (get-top-level-dir current-dir)
+             size size]
+        (let [current-dir-size (get dirs current-dir 0)]
+          (if (= current-dir "/")
+            (assoc dirs "/" (+ current-dir-size size))
+            (recur (assoc dirs current-dir (+ current-dir-size size)) (get-top-level-dir current-dir) size)))))))
+
+(defn- sum-directory-sizes
+  [dirs]
+  (loop [keys (keys dirs)
+         dirs dirs]
+    (if (empty? keys)
+      dirs
+      (recur (rest keys) (update-directory-sizes dirs (first keys))))))
+
+(defn part1
+  [data]
+  (->> data
+      process-command-log
+      sum-directory-sizes
+      vals
+      (filter #(< % 100000))
+      (reduce +)))
+
+(defn part2
+  [data]
+  (let [dirs (sum-directory-sizes (process-command-log data))
+        unused-space (- 70000000 (get dirs "/"))
+        required-space (- 30000000 unused-space)]
+  (->> (vals dirs)
+       (sort >)
+       (take-while #(> % required-space))
+       last)))
+
+(defn -main
+  []
+  (let [day "07"
+        data (read-input-as-string-vector (str "day" day ".txt"))]
+    (printf "Day %s, part 1: %s\n", day, (part1 data))
+    (printf "Day %s, part 2: %s\n", day, (part2 data))))
---
+++ test/advent_of_code_2022/day07_test.clj
@@ -0,0 +1,36 @@
+(ns advent-of-code-2022.day07-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [advent-of-code-2022.day07 :refer [part1 part2]]))
+
+(def command-log-example [
+  "$ cd /"
+  "$ ls"
+  "dir a"
+  "14848514 b.txt"
+  "8504156 c.dat"
+  "dir d"
+  "$ cd a"
+  "$ ls"
+  "dir e"
+  "29116 f"
+  "2557 g"
+  "62596 h.lst"
+  "$ cd e"
+  "$ ls"
+  "584 i"
+  "$ cd .."
+  "$ cd .."
+  "$ cd d"
+  "$ ls"
+  "4060174 j"
+  "8033020 d.log"
+  "5626152 d.ext"
+  "7214296 k"])
+
+(deftest part1-test
+  (testing "Part 1"
+    (is (= 95437 (part1 command-log-example)))))
+
+(deftest part2-test
+  (testing "Part 2"
+    (is (= 24933642 (part2 command-log-example)))))
