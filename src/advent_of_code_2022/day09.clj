4b4bfae62226dc11bc961d635b3ee7171ff62623
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2022-12-09 09:44:26 +0100

    feat: Solution for day 09 (parts 1 and 2)

--- src/advent_of_code_2022/day09.clj
+++ src/advent_of_code_2022/day09.clj
@@ -8,43 +8,66 @@
        (map rest)
        (map #(list (first %) (Integer/parseInt (second %))))))
 
-(defn- process-step
-  [tail-visited head-pos tail-pos instruction]
+(defn- get-new-head-pos
+  [head-pos dir]
+  (let [[hx hy] head-pos]
+    (case
+      dir "U" [hx (inc hy)]
+          "R" [(inc hx) hy]
+          "D" [hx (dec hy)]
+          "L" [(dec hx) hy])))
+
+(defn- get-new-tail-pos
+  [new-head-pos tail-pos]
+  (let [[nhx nhy] new-head-pos
+        [tx ty] tail-pos]
+    (if (and (<= (abs (- nhx tx)) 1) (<= (abs (- nhy ty)) 1))
+      [tx ty]
+      [(if (= nhx tx) tx (+ tx (/ (- nhx tx) (abs (- nhx tx)))))
+       (if (= nhy ty) ty (+ ty (/ (- nhy ty) (abs (- nhy ty)))))])))
+
+(defn- process-single-step
+  [rope dir]
+  (loop [new-head-pos (get-new-head-pos (first rope) dir)
+         new-rope [new-head-pos]
+         rope (rest rope)]
+    (if (empty? rope)
+      new-rope
+      (let [new-tail-pos (get-new-tail-pos new-head-pos (first rope))]
+        (recur new-tail-pos (conj new-rope new-tail-pos) (rest rope))))))
+
+(defn- process-single-instruction
+  [rope instruction]
   (let [[dir val] instruction]
-  (loop [visited tail-visited
-        [hx hy]  head-pos
-        [tx ty] tail-pos
-        val val]
-    (if (= val 0)
-      [visited [hx hy] [tx ty]]
-      (let [[nhx nhy] (case dir "U" [hx (inc hy)] "R" [(inc hx) hy] "D" [hx (dec hy)] "L" [(dec hx) hy])
-            ntx (if (= nhx tx) tx (+ tx (/ (- nhx tx) (abs (- nhx tx)))))
-            nty (if (= nhy ty) ty (+ ty (/ (- nhy ty) (abs (- nhy ty)))))]
-        (cond
-          (and (<= (abs (- nhx tx)) 1) (<= (abs (- nhy ty)) 1)) (recur visited [nhx nhy] [tx ty] (dec val))
-          (and (= nhx tx) (> nhy ty)) [(into visited (map #(vector ntx %) (range nty (+ nty val)))) [nhx (+ nhy (dec val))] [ntx (+ nty (dec val))]]
-          (and (> nhx tx) (= nhy ty)) [(into visited (map #(vector % nty) (range ntx (+ ntx val)))) [(+ nhx (dec val)) nhy] [(+ ntx (dec val)) nty]]
-          (and (= nhx tx) (< nhy ty)) [(into visited (map #(vector ntx %) (range nty (- nty val) -1))) [nhx (- nhy (dec val))] [ntx (- nty (dec val))]]
-          (and (< nhx tx) (= nhy ty)) [(into visited (map #(vector % nty) (range ntx (- ntx val) -1))) [(- nhx (dec val)) nhy] [(- ntx (dec val)) nty]]
-          :else (recur (conj visited [ntx nty]) [nhx nhy] [ntx nty] (dec val))
-          )
-        ))
-      )))
+    (loop [tail-visited #{}
+           rope rope
+           val val]
+      (if (= val 0)
+        [tail-visited rope]
+        (let [new-rope (process-single-step rope dir)]
+          (recur (conj tail-visited (last new-rope)) new-rope (dec val)))))))
+
+(defn- process-instructions
+  [instructions rope-size]
+  (loop [instructions instructions
+         tail-visited #{[0 0]}
+         rope (take rope-size (repeat [0 0]))]
+    (if (empty? instructions)
+      (count tail-visited)
+      (let [[new-tail-visited new-rope] (process-single-instruction rope (first instructions))]
+        (recur (rest instructions) (into tail-visited new-tail-visited) new-rope)))))
 
 (defn part1
   [data]
-  (loop [instructions (parse-instructions data)
-         visited #{[0 0]}
-         head-pos [0 0]
-         tail-pos [0 0]]
-    (if (empty? instructions)
-      (count visited)
-      (let [[visited head-pos tail-pos] (process-step visited head-pos tail-pos (first instructions))]
-        (recur (rest instructions) visited head-pos tail-pos)))))
+  (-> data
+      parse-instructions
+      (process-instructions 2)))
 
 (defn part2
   [data]
-  nil)
+  (-> data
+      parse-instructions
+      (process-instructions 10)))
 
 (defn -main
   []
@@ -52,16 +75,3 @@
         data (read-input-as-string-vector (str "day" day ".txt"))]
     (printf "Day %s, part 1: %s\n", day, (part1 data))
     (printf "Day %s, part 2: %s\n", day, (part2 data))))
-
-(def instructions-example ["R 4"
-                           "U 4"
-                           "L 3"
-                           "D 1"
-                           "R 4"
-                           "D 1"
-                           "L 5"
-                           "R 2"])
-
-
-
-(part1 instructions-example)
--- test/advent_of_code_2022/day09_test.clj
+++ test/advent_of_code_2022/day09_test.clj
@@ -2,19 +2,29 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent-of-code-2022.day09 :refer [part1 part2]]))
 
-(def instructions-example ["R 4"
-                           "U 4"
-                           "L 3"
-                           "D 1"
-                           "R 4"
-                           "D 1"
-                           "L 5"
-                           "R 2"])
+(def small-instructions-example ["R 4"
+                                 "U 4"
+                                 "L 3"
+                                 "D 1"
+                                 "R 4"
+                                 "D 1"
+                                 "L 5"
+                                 "R 2"])
+
+(def large-instructions-example ["R 5"
+                                 "U 8"
+                                 "L 8"
+                                 "D 3"
+                                 "R 17"
+                                 "D 10"
+                                 "L 25"
+                                 "U 20"])
 
 (deftest part1-test
   (testing "Part 1"
-    (is (= 13 (part1 instructions-example)))))
+    (is (= 13 (part1 small-instructions-example)))))
 
 (deftest part2-test
   (testing "Part 2"
-    (is (= nil (part2 "def")))))
+    (is (= 1 (part2 small-instructions-example)))
+    (is (= 36 (part2 large-instructions-example)))))
