49ed16cb6a69e421d46f7165385ad3dd1665c6b1
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2022-12-14 21:25:06 +0100

    refactor: Simplify parsing

--- src/advent_of_code_2022/day14.clj
+++ src/advent_of_code_2022/day14.clj
@@ -1,28 +1,26 @@
 (ns advent-of-code-2022.day14
   (:require [advent-of-code-2022.utils :refer [read-input-as-string-vector]]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.string :as str]))
 
 (defn- parse-cave-data-line
   [line]
-  (loop [cave {}
+  (loop [cave #{}
          points (str/split line #" -> ")]
     (if (= (count points) 1)
       cave
       (let [[lx, ly] (map #(Integer/parseInt %) (str/split (first points) #","))
-            [rx, ry] (map #(Integer/parseInt %) (str/split (second points) #","))
-            new-rocks (if (= lx rx)
-                        [{lx (into #{} (range (min ly ry) (inc (max ly ry))))}]
-                        (map #(assoc {} % #{ly}) (range (min lx rx) (inc (max lx rx)))))]
-        (recur (reduce #(merge-with into %1 %2) cave new-rocks) (rest points))))))
+            [rx, ry] (map #(Integer/parseInt %) (str/split (second points) #","))]
+        (if (= lx rx)
+          (recur (into cave (map #(vector lx %) (range (min ly ry) (inc (max ly ry))))) (rest points))
+          (recur (into cave (map #(vector % ly) (range (min lx rx) (inc (max lx rx))))) (rest points)))))))
 
 (defn- parse-cave-data
   [data]
-  (loop [cave {}
+  (loop [cave #{}
          lines data]
     (if (= (count lines) 0)
       cave
-      (recur (merge-with into cave (parse-cave-data-line (first lines))) (rest lines)))))
+      (recur (into cave (parse-cave-data-line (first lines))) (rest lines)))))
 
 (defn- process-send-tile
   [cave floor-y x y]
@@ -31,20 +29,20 @@
          y y]
     (cond
       (= y floor-y) [x (dec floor-y)]
-      (not (contains? (get cave x) (inc y))) (recur cave x (inc y))
-      (not (contains? (get cave (dec x)) (inc y))) (recur cave (dec x) (inc y))
-      (not (contains? (get cave (inc x)) (inc y))) (recur cave (inc x) (inc y))
+      (not (contains? cave [x (inc y)])) (recur cave x (inc y))
+      (not (contains? cave [(dec x) (inc y)])) (recur cave (dec x) (inc y))
+      (not (contains? cave [(inc x) (inc y)])) (recur cave (inc x) (inc y))
       :else [x y])))
 
 (defn- process-send
   [cave floor-stop-condition?]
-  (let [floor-y (+ (apply max (apply set/union (vals cave))) 2)]
+  (let [floor-y (+ (apply max (map second cave)) 2)]
     (loop [cave cave
            round 1]
       (let [[x y] (process-send-tile cave floor-y 500 0)]
         (if (floor-stop-condition? y floor-y)
           round
-          (recur (merge-with into cave {x #{y}}) (inc round)))))))
+          (recur (conj cave [x y]) (inc round)))))))
 
 (defn part1
   [data]
