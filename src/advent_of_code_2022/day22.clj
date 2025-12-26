ae76d8905a5af9aeafd2373033e131f2435c919b
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2025-12-26 21:05:28 +0100

    Day 22, part 2 by manually encode the cube.

--- src/advent_of_code_2022/day22.clj
+++ src/advent_of_code_2022/day22.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2022.day22
   (:require [advent-of-code-2022.utils :refer [read-input-as-string-vector]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set]))
 
 (def directions {:E [0 1]
                  :S [1 0]
@@ -10,7 +11,7 @@
 (defn- parse-monkey-map
   [data]
   (->> data
-       (map-indexed #(vector (inc %1) (map-indexed (fn [idx val] (vector (inc idx) val)) %2)))
+       (map-indexed #(vector %1 (map-indexed (fn [idx val] (vector idx val)) %2)))
        (map #(map (fn [chr] (vector [(first %) (first chr)] (second chr))) (second %)))
        (apply concat)
        (remove #(= (last %) \space))
@@ -29,6 +30,19 @@
   (let [data (remove empty? data)]
     [(parse-monkey-map (drop-last data)) (parse-path (last data))]))
 
+(defn- enrich-scheme-with-coords
+  [scheme monkey-map]
+  (let [size (:side-size scheme)
+        positions (for [y (range 0 (inc size)) x (range 0 (inc size))] [(* y size) (* x size)])]
+    (loop [pos (first positions)
+           tail (rest positions)
+           side 1
+           stn {}]
+      (cond
+        (nil? pos) (assoc scheme :side-to-coord stn)
+        (contains? monkey-map pos) (recur (first tail) (rest tail) (inc side) (assoc stn side pos))
+        :else (recur (first tail) (rest tail) side stn)))))
+
 (defn- rotate
   [current-dir rotation]
   (cond
@@ -37,32 +51,94 @@
     (= (:W directions) current-dir) (if (= rotation "L") (:S directions) (:N directions))
     (= (:N directions) current-dir) (if (= rotation "L") (:W directions) (:E directions))))
 
-(defn- get-wrap-pos
+(defn- get-wrap-flat-pos
   [monkey-map pos dir]
   (let [ks (keys monkey-map)]
     (cond
-      (= (:E directions) dir) (apply min-key second (filter #(= (first %) (first pos)) ks))
-      (= (:S directions) dir) (apply min-key first (filter #(= (second %) (second pos)) ks))
-      (= (:W directions) dir) (apply max-key second (filter #(= (first %) (first pos)) ks))
-      (= (:N directions) dir) (apply max-key first (filter #(= (second %) (second pos)) ks)))))
+      (= (:E directions) dir) [(apply min-key second (filter #(= (first %) (first pos)) ks)) dir]
+      (= (:S directions) dir) [(apply min-key first (filter #(= (second %) (second pos)) ks)) dir]
+      (= (:W directions) dir) [(apply max-key second (filter #(= (first %) (first pos)) ks)) dir]
+      (= (:N directions) dir) [(apply max-key first (filter #(= (second %) (second pos)) ks)) dir])))
+
+(defn- invert-direction
+  [dir]
+  (cond
+    (= dir :N) :S
+    (= dir :W) :E
+    (= dir :S) :N
+    (= dir :E) :W))
+
+(defn- pos-to-side
+  [scheme pos]
+  (let [size (:side-size scheme)
+        side-map (:side-to-coord scheme)
+        [y x] pos
+        ny (- y (mod y size))
+        nx (- x (mod x size))]
+    (loop [side 1]
+      (let [[cy cx] (get side-map side)]
+        (if (and (= ny cy) (= nx cx)) side (recur (inc side)))))))
+
+(defn- change-side
+  [scheme pos dir]
+  (let [[y x] pos
+        size (:side-size scheme)
+        dsize (dec size)
+        stn (:side-to-neighbors scheme)
+        cur-side (pos-to-side scheme pos)
+        [cy cx] (get (:side-to-coord scheme) cur-side)
+        [dy dx] [(- y cy) (- x cx)]
+        next-side (get (get stn cur-side) dir)
+        [ny nx] (get (:side-to-coord scheme) next-side)
+        next-dir (get (clojure.set/map-invert (get stn next-side)) cur-side)
+        next-inverted-dir (invert-direction next-dir)
+        ]
+    (cond
+      (= [dir next-dir] [:N :N]) [[ny (+ nx (- dsize dx))] next-inverted-dir]
+      (= [dir next-dir] [:N :W]) [[(+ ny dx) nx] next-inverted-dir]
+      (= [dir next-dir] [:N :S]) [[(+ ny dsize) (+ nx dx)] next-inverted-dir]
+      (= [dir next-dir] [:N :E]) [[(+ ny (- dsize dx)) (+ nx dsize)] next-inverted-dir]
+
+      (= [dir next-dir] [:W :N]) [[ny (+ nx dy)] next-inverted-dir]
+      (= [dir next-dir] [:W :W]) [[(+ ny (- dsize dy)) nx] next-inverted-dir]
+      (= [dir next-dir] [:W :S]) [[(+ ny dsize) (+ nx (- dsize dy))] next-inverted-dir]
+      (= [dir next-dir] [:W :E]) [[(+ ny dy) (+ nx dsize)] next-inverted-dir]
+
+      (= [dir next-dir] [:S :N]) [[ny (+ nx dx)] next-inverted-dir]
+      (= [dir next-dir] [:S :W]) [[(+ ny (- dsize dx)) nx] next-inverted-dir]
+      (= [dir next-dir] [:S :S]) [[(+ ny dsize) (+ nx (- dsize dx))] next-inverted-dir]
+      (= [dir next-dir] [:S :E]) [[(+ ny dx) (+ nx dsize)] next-inverted-dir]
+
+      (= [dir next-dir] [:E :N]) [[ny (+ nx (- dsize dy))] next-inverted-dir]
+      (= [dir next-dir] [:E :W]) [[(+ ny dy) nx] next-inverted-dir]
+      (= [dir next-dir] [:E :S]) [[(+ ny dsize) (+ nx dy)] next-inverted-dir]
+      (= [dir next-dir] [:E :E]) [[(+ ny (- dsize dy)) (+ nx dsize)] next-inverted-dir])
+    ))
+
+(defn- get-wrap-cube-pos
+  [scheme monkey-map pos dir]
+  (let [dir-symbol (get (clojure.set/map-invert directions) dir)
+        [new-pos new-dir-symbol] (change-side scheme pos dir-symbol)]
+    [new-pos (get directions new-dir-symbol)]))
 
 (defn- move
-  [monkey-map pos dir val]
+  [monkey-map wrap-fn pos dir val]
   (loop [pos pos
+         dir dir
          steps val]
     (let [[y x] pos
           [dy dx] dir
           new-pos (vector (+ y dy) (+ x dx))
-          new-pos (if (contains? monkey-map new-pos) new-pos (get-wrap-pos monkey-map pos dir))]
+          [new-pos new-dir] (if (contains? monkey-map new-pos) [new-pos dir] (wrap-fn monkey-map pos dir))]
       (cond
-        (= steps 0) pos
-        (= (get monkey-map new-pos) \#) pos
-        (= (get monkey-map new-pos) \.) (recur new-pos (dec steps))))))
+        (= steps 0) [pos dir]
+        (= (get monkey-map new-pos) \#) [pos dir]
+        (= (get monkey-map new-pos) \.) (recur new-pos new-dir (dec steps))))))
 
 (defn- follow-path
-  [monkey-map monkey-path]
+  [monkey-map monkey-path wrap-fn]
   (let [initial-position (->> monkey-map
-                              (filter #(= (first (first %)) 1))
+                              (filter #(= (first (first %)) 0))
                               (remove #(= (last %) \#))
                               (map first)
                               (apply min-key second))
@@ -75,22 +151,42 @@
           (empty? path) [pos dir]
           (= next "L") (recur (rest path) pos (rotate dir next))
           (= next "R") (recur (rest path) pos (rotate dir next))
-          :else (recur (rest path) (move monkey-map pos dir next) dir))))))
+          :else (let [[new-pos new-dir] (move monkey-map wrap-fn pos dir next)]
+                  (recur (rest path) new-pos new-dir)))))))
 
 (defn part1
   [data]
   (let [[monkey-map monkey-path] (parse-monkey-data data)
-        [[y x] dir] (follow-path monkey-map monkey-path)
-        dir-val (cond (= (:E directions) dir) 0 (= (:S directions) dir) 1 (= (:W directions) dir) 2 (= (:N directions) dir) 3)]
-    (+ (* y 1000) (* x 4) dir-val)))
+        [[y x] dir] (follow-path monkey-map monkey-path get-wrap-flat-pos)
+        dir-val (cond
+                  (= (:E directions) dir) 0
+                  (= (:S directions) dir) 1
+                  (= (:W directions) dir) 2
+                  (= (:N directions) dir) 3)]
+    (+ (* (inc y) 1000) (* (inc x) 4) dir-val)))
 
 (defn part2
-  [data]
-  nil)
+  [data cube-scheme]
+  (let [[monkey-map monkey-path] (parse-monkey-data data)
+        scheme (enrich-scheme-with-coords cube-scheme monkey-map)
+        [[y x] dir] (follow-path monkey-map monkey-path (partial get-wrap-cube-pos scheme))
+        dir-val (cond
+                  (= (:E directions) dir) 0
+                  (= (:S directions) dir) 1
+                  (= (:W directions) dir) 2
+                  (= (:N directions) dir) 3)]
+    (+ (* (inc y) 1000) (* (inc x) 4) dir-val)))
 
 (defn -main
   []
   (let [day "22"
-        data (read-input-as-string-vector (str "day" day ".txt"))]
+        data (read-input-as-string-vector (str "day" day ".txt"))
+        cube-scheme {:side-size         50
+                     :side-to-neighbors {1 {:N 6, :W 4, :S 3, :E 2}
+                                         2 {:N 6, :W 1, :S 3, :E 5}
+                                         3 {:N 1, :W 4, :S 5, :E 2}
+                                         4 {:N 3, :W 1, :S 6, :E 5}
+                                         5 {:N 3, :W 4, :S 6, :E 2}
+                                         6 {:N 4, :W 1, :S 2, :E 5}}}]
     (printf "Day %s, part 1: %s\n", day, (part1 data))
-    (printf "Day %s, part 2: %s\n", day, (part2 data))))
+    (printf "Day %s, part 2: %s\n", day, (part2 data cube-scheme))))
--- test/advent_of_code_2022/day22_test.clj
+++ test/advent_of_code_2022/day22_test.clj
@@ -17,10 +17,18 @@
                          ""
                          "10R5L5R10L4R5L5"])
 
+(def cube-scheme {:side-size         4
+                  :side-to-neighbors {1 {:N 2, :W 3, :S 4, :E 6}
+                                      2 {:N 1, :W 6, :S 5, :E 3}
+                                      3 {:N 1, :W 2, :S 5, :E 4}
+                                      4 {:N 1, :W 3, :S 5, :E 6}
+                                      5 {:N 4, :W 3, :S 2, :E 6}
+                                      6 {:N 4, :W 5, :S 2, :E 1}}})
+
 (deftest part1-test
   (testing "Part 1"
     (is (= 6032 (part1 monkey-map-example)))))
 
 (deftest part2-test
   (testing "Part 2"
-    (is (= nil (part2 "def")))))
+    (is (= 5031 (part2 monkey-map-example cube-scheme)))))
